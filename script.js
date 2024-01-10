5179c495a9615eb92e57da10fc8c82ed29f8f72f
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2024-01-10 22:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function fibonacci(num) {
-// your code here
+function fibonacci(n) {
+   if(n <= 2) return n-1; 
+    let a = 0, b = 1 ; 
+
+    // n = 3
+    while(n > 2) {
+        let curr = a + b; 
+        a = b; 
+        b = curr;
+        n--;
+    }
+
+    return b;
 }
 
 module.exports = fibonacci;
